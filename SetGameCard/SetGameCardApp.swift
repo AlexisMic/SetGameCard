96a050a8c3ed46181c6999c666a330b0af9211f5
AlexisMic <amschotte@me.com>
2022-08-15 18:19:41 +0100

    Initial Commit

---
+++ SetGameCard/SetGameCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameCardApp.swift
+//  SetGameCard
+//
+//  Created by Alexis Schotte on 8/15/22.
+//
+
+import SwiftUI
+
+@main
+struct SetGameCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
